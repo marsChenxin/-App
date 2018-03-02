df56099d2f800af9510d1e098d060f47e7cf1d99
陈鑫 <30886774+marsChenxin@users.noreply.github.com>
2018-03-02 12:56:24 +0800

    Add files via upload

---
+++ src/components/cartControl/cartControl.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="inner icon-offline"></span>
+      </div>
+    </transition>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-addition" @click="addCart"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue';
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart (event) {
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+      },
+      decreaseCart (event) {
+        if (this.food.count) {
+          this.food.count--;
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontrol
+    font-size: 0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      opacity: 1
+      transform: translate3d(0, 0, 0)
+      .inner
+        display: inline-block
+        line-height: 28px
+        font-size: 28px
+        color: rgb(0, 160, 220)
+        transition: all 0.4s linear
+        transform: rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.4s linear
+      &.move-enter, &.move-leave-active
+        opacity: 0
+        transform: translate3d(28px, 0, 0)
+        .inner
+          transform: rotate(180deg)
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      padding-top: 6px
+      line-height: 28px
+      text-align: center
+      font-size: 14px
+      font-weight 700
+      color: rgb(147, 153, 159)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 28px
+      font-size: 28px
+      color: rgb(0, 160, 220)
+</style>
